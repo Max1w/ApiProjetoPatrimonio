15ac146a56597e6ad582a49dbc236f154fe20bc1
Max Willian <max.willian@moveresoftware.com>
2024-06-19 14:35:53 -0400

    Atualizando modelo

--- Patrimonio/Modelos/PatrimonioItens.cs
+++ Patrimonio/Modelos/PatrimonioItens.cs
@@ -7,51 +7,79 @@ namespace Patrimonio.Modelos
 	public class PatrimonioItens
 	{
 		public int Id { get; set; }
+
 		[Required]
-		public string codigo_item { get; set; }
+		public string? codigo_item { get; set; } = "";
+
 		[Required]
-		public string placa_item { get; set; }
+		public string? placa_item { get; set; } = "";
+
 		[Required]
-		public string descricao_item { get; set; }
+		public string? descricao_item { get; set; } = "";
+
 		[Required]
-		public string tipo_item { get; set; }
+		public string? tipo_item { get; set; } = "";
+
 		[Required]
-		public string grupo_item { get; set; }
+		public string? grupo_item { get; set; } = "";
+
 		[Required]
-		public string estado_conservacao { get; set; }
+		public string? estado_conservacao { get; set; } = "";
+
 		[Required]
-		public string tipo_aquisicao { get; set; }
+		public string? tipo_aquisicao { get; set; } = "";
+
 		[Required]
-		public string valor_aquisicao { get; set; }
+		public string? valor_aquisicao { get; set; } = "";
+
 		[Required]
-		public string metodo_depreciacao { get; set; }
+		public string? metodo_depreciacao { get; set; } = "";
+
 		[Required]
-		public string valor_residual { get; set; }
+		public string? valor_residual { get; set; } = "";
+
 		[Required]
-		public string responsavel { get; set; }
+		public string? responsavel { get; set; } = "";
+
 		[Required]
-		public string vida_util { get; set; }
+		public string? vida_util { get; set; } = "";
+
 		[Required]
-		public string depreciacao_anual { get; set; }
+		public string? depreciacao_anual { get; set; } = "";
+
 		[Required]
 		public DateTime inicio_depreciacao { get; set; }
+
 		[Required]
 		public DateTime data_aquisicao { get; set; }
-		public string valor_depreciavel { get; set; }
+
 		[Required]
-		public string valor_depreciado { get; set; }
+		public string? valor_depreciavel { get; set; } = "";
+
 		[Required]
-		public string saldo_depreciar { get; set; }
+		public string? valor_depreciado { get; set; } = "";
+
 		[Required]
-		public string valor_liquido { get; set; }
-		public string tipo_comprovante { get; set; }
-		public string numero_comprovante { get; set; }
-		public string tem_combustivel { get; set; }
-		public string placa_veiculo { get; set; }
-		public string modelo_veiculo { get; set; }
-		public string localizacao_fisica { get; set; }
-		public string observacao { get; set; }
+		public string? saldo_depreciar { get; set; } = "";
+
 		[Required]
-		public int patrimonios_id { get; set; }
+		public string? valor_liquido { get; set; } = null;
+
+		public string? tipo_comprovante { get; set; } = null;
+
+		public string? numero_comprovante { get; set; } = null;
+
+		public string? tem_combustivel { get; set; } = null;
+
+		public string? placa_veiculo { get; set; } = null;
+
+		public string? modelo_veiculo { get; set; } = null;
+
+		public string? localizacao_fisica { get; set; } = null;
+
+		public string? observacao { get; set; } = null;
+
+		[Required]
+		public int patrimonios_id { get; set; } = 1;
 	}
 }
